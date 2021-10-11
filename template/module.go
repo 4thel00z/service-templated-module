3fdaa293b2dc99ed7b8e3174950799bc99447253
4thel00z <4thel00z@gmail.com>
2021-10-11 20:08:51 +0200

    [master] Fix hardcoded name, remove wrongly hardcoded Route
    Signed-off-by: 4thel00z <4thel00z@gmail.com>

--- template/module.go
+++ template/module.go
@@ -3,8 +3,6 @@
 // License     {{ License }}
 // Copyright   {{ Year }} by {{ Author }}
 
-
-
 package {{ lower Name }}
 
 import (
@@ -22,19 +20,12 @@ func (d {{ title Name }}) Version() string {
 }
 
 func (d {{ title Name }}) Namespace() string {
-	return "debug"
+	return "{{ lower Name }}"
 }
 
 func (d {{ title Name }}) Routes() map[string]libservice.Route {
 	// Add route definitions here
-	return map[string]libservice.Route{
-		"routes": {
-			Path:        "routes",
-			Method:      "GET",
-			CurlExample: "curl http://<addr>/<version>/<namespace>/<path>",
-			Service:     GetRoutesHandler,
-		},
-	}
+	return map[string]libservice.Route{}
 }
 
 func (d {{ title Name }}) LongPath(route libservice.Route) string {
